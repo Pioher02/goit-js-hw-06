bc28d2ae20db6b9b4e1e5ecbf93b56fd89136543
Pioher02 <dpaola.hernandezr@gmail.com>
2022-12-02 22:04:19 -0500

    validación tarea 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,17 +5,19 @@ const validate = () => {
     const counter = input.value;
     
     if (counter.length == dataLength) {
+        console.log("Counter tiene 6 caracteres");
+
         if (input.classList.contains("invalid")) {
-            
+            console.log("existe la clase invalid se cambia");
             input.classList.replace("invalid", "valid");
             
         } else {
-            
+            console.log("no existe invalid");
             input.classList.add("valid");  
         }
            
     } else {
-        
+        console.log("Counter NO TIENE 6 caracteres");
         input.classList.add("invalid");
     }
         
