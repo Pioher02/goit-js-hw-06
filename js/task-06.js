24b4c32e5c9c63ed9415fcb1d3c60b80efcf7f0c
Pioher02 <dpaola.hernandezr@gmail.com>
2022-12-02 21:54:57 -0500

    corrección tareas 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,24 @@
-const input = document.querySelector("#validation-input");
-
+const input = document.getElementById("validation-input");
+const dataLength = input.dataset.length;
 
 const validate = () => {
     const counter = input.value;
-       
-    if (counter.length === 6) {
-       
-        input.classList.add("valid");
-      
+    
+    if (counter.length == dataLength) {
+        if (input.classList.contains("invalid")) {
+            
+            input.classList.replace("invalid", "valid");
+            
+        } else {
+            
+            input.classList.add("valid");  
+        }
+           
     } else {
+        
         input.classList.add("invalid");
     }
-    
-    
+        
   };
 
 input.addEventListener("blur", validate);
-
